05642347f04c161876b3c5307280623a04aac7cf
Aly Fluckey <afluckey@mozilla.com>
2023-10-04 14:55:31 -0400

    feat(glean): adjust nuxt options for glean module

--- modules/glean/index.ts
+++ modules/glean/index.ts
@@ -1,15 +1,11 @@
-import { addPlugin, createResolver, defineNuxtModule, useNuxt } from '@nuxt/kit'
+import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: 'glean',
   },
   setup() {
-    const nuxt = useNuxt()
     const { resolve } = createResolver(import.meta.url)
-
-    nuxt.options.pwa.disable = true
-    nuxt.options.sourcemap.client = false
 
     addPlugin(resolve('./runtime/glean-plugin.client'))
   },
