46eb2301fa177f69b3ae5eb72d85d29265c30d5d
Aly Fluckey <afluckey@mozilla.com>
2023-10-04 11:10:45 -0400

    feat(glean): set up glean module & plugin

---
+++ modules/glean/runtime/glean-plugin.client.ts
@@ -0,0 +1,29 @@
+import Glean from '@mozilla/glean/web'
+import * as log from 'tauri-plugin-log-api'
+
+import { load } from '../../../telemetry/generated/page'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('app:mounted', () => {
+    log.info('Glean: App mounted, start initing glean')
+
+    const GLEAN_APP_ID = 'mozilla-social-web'
+    const devMode = useAppConfig().env === 'dev'
+    const uploadEnabled = true // this will eventually be a setting that the user can toggle
+
+    Glean.initialize(GLEAN_APP_ID, uploadEnabled, { })
+
+    // Debugging
+    if (devMode) {
+      Glean.setLogPings(true) // logs to the console
+      Glean.setDebugViewTag('moso-elk-dev') // logs to https://debug-ping-preview.firebaseapp.com
+    }
+  })
+
+  nuxtApp.hook('page:finish', () => {
+    log.info('Glean: Page finished, sending page event')
+    const route = useRoute()
+
+    load.record({ path: route.path })
+  })
+})
