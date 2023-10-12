b560b49641ad2899ca45dae802797b4cd59b0b81
Aly Fluckey <afluckey@mozilla.com>
2023-10-12 17:04:43 -0400

    fix(invite page): add title & icon

--- pages/invites.vue
+++ pages/invites.vue
@@ -6,14 +6,17 @@ definePageMeta({
 const { t } = useI18n()
 
 useHydratedHead({
-  title: () => t('nav.invitation_tokens'),
+  title: () => t('nav.invites'),
 })
 </script>
 
 <template>
   <MainContent>
     <template #title>
-      <NuxtLink to="/invites" timeline-title-style flex items-center gap-2 @click="$scrollToTop" />
+      <NuxtLink to="/invites" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
+        <div i-iconamoon:ticket />
+        <span>{{ $t('nav.invites') }}</span>
+      </NuxtLink>
     </template>
     <SettingsInviteCodes v-if="isHydrated" />
   </MainContent>
