9e71c8892077b892943a73d818fafac845654a62
Daniel Brooks <bass_rock@me.com>
2023-10-12 15:13:00 -0700

    fix(initial): adding in initial dummy page (#62)
    * fix(initial): adding in initial dummy page
    
    * fix(codes): adding in the codes
    
    * fix(codes): updating render
    
    * fix(nav): update icon for invites link
    
    * fix(invite page): add title & icon
    
    * fix(invites): style invites page, add copy to clipboard
    
    * fix(invites): translation string
    
    * fix(invites): update no_invites copy
    
    ---------
    
    Co-authored-by: Aly Fluckey <afluckey@mozilla.com>

--- pages/invites.vue
+++ pages/invites.vue
@@ -6,21 +6,18 @@ definePageMeta({
 const { t } = useI18n()
 
 useHydratedHead({
-  title: () => t('nav.invitation_tokens'),
-})
-
-const loggedInUsers = useUsers()
-const data = await fetch(`https://${loggedInUsers.value.server}/api/invitation/v1/status`, {
-  headers: { authorization: `Bearer ${loggedInUsers.value.token}` },
+  title: () => t('nav.invites'),
 })
 </script>
 
 <template>
   <MainContent>
     <template #title>
-      <NuxtLink to="/invitations" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
-        <span>{{ JSON.stringify(data, null, 3) }}</span>
+      <NuxtLink to="/invites" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
+        <div i-iconamoon:ticket />
+        <span>{{ $t('nav.invites') }}</span>
       </NuxtLink>
     </template>
+    <SettingsInviteCodes v-if="isHydrated" />
   </MainContent>
 </template>
